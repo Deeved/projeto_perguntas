3b1709981c55f615dcdc33b3a32d01ef3651f985
Deeved Hiuston <deeved@gmail.com>
2022-04-03 22:23:34 -0300

    Funções anônimas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 main() => runApp(const PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({Key? key}) : super(key: key);
+
+  void responder() {
+    print('Pergunta respondida!');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +24,18 @@ class PerguntaApp extends StatelessWidget {
         title: const Text('Perguntas'),
       ),
       body: Column(
-        children: [
+        children: <Widget>[
           Text(perguntas[0]),
-          RaisedButton(
-            child: Text('Resposta 1'),
-            onPressed: null,
-          ),
-          RaisedButton(
+          ElevatedButton(child: Text('Resposta 1'), onPressed: responder),
+          ElevatedButton(
             child: Text('Resposta 2'),
-            onPressed: null,
+            onPressed: () {
+              print('Resposta 2 foi selecionada');
+            },
           ),
-          RaisedButton(
+          ElevatedButton(
             child: Text('Resposta 3'),
-            onPressed: null,
+            onPressed: () => print('Resposta 3!!!'),
           )
         ],
       ),
