819cf7d6c5ff7607e486059cadf6e9f111601af5
Deeved Hiuston <deeved@gmail.com>
2022-04-04 21:02:16 -0300

    Variáveis em StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@
 
 import 'package:flutter/material.dart';
 
-main() => runApp(const PerguntaApp());
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({Key? key}) : super(key: key);
-
+  // const PerguntaApp({Key? key}) : super(key: key);
+  
   void responder() {
-    print('Pergunta respondida!');
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
   }
+
+  var perguntaSelecionada = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +22,21 @@ class PerguntaApp extends StatelessWidget {
     ];
 
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Perguntas'),
-      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
       body: Column(
         children: <Widget>[
-          Text(perguntas[0]),
+          Text(perguntas[perguntaSelecionada]),
           ElevatedButton(child: Text('Resposta 1'), onPressed: responder),
           ElevatedButton(
             child: Text('Resposta 2'),
-            onPressed: () {
-              print('Resposta 2 foi selecionada');
-            },
+            onPressed: responder,
           ),
           ElevatedButton(
             child: Text('Resposta 3'),
-            onPressed: () => print('Resposta 3!!!'),
+              onPressed: responder,
           )
         ],
       ),
