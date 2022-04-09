00bf467772becb6168545e54ee120cabc63ccd34
Deeved Hiuston <deeved@gmail.com>
2022-04-08 21:00:46 -0300

    Adicionando pontuação 3

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                     perguntas: _perguntas,
                     perguntaSelecionada: _perguntaSelecionada,
                     quandoResponder: _responder)
-                : Resultado()));
+                : Resultado(_pontuacaoTotal)));
   }
 }
 
--- lib/resultado.dart
+++ lib/resultado.dart
@@ -2,11 +2,26 @@ import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
 
+  final int pontuacao;
+
+  Resultado(this.pontuacao);
+
+  String get fraseResultdo {
+    if(pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return 'você é bom!';
+    } else if (pontuacao < 16) {
+      return 'Impressionante!';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Parabéns!', 
+      child: Text(fraseResultdo, 
       style: TextStyle(fontSize: 28)
       )
     );
